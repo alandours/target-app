ab2dc19fe69d0822ba8bc68cb486348659c54733
Alan Dours <alan.dours@rootstrap.com>
2021-12-06 15:20:18 -0300

    Set headerShown to false in Navigator

--- src/navigators/AuthStack.js
+++ src/navigators/AuthStack.js
@@ -9,9 +9,9 @@ import SignUpScreen from 'screens/SignUpScreen';
 const Stack = createStackNavigator();
 
 const AuthStack = () => (
-  <Stack.Navigator>
-    <Stack.Screen name={LOGIN_SCREEN} component={LoginScreen} options={{ headerShown: false }} />
-    <Stack.Screen name={SIGN_UP_SCREEN} component={SignUpScreen} options={{ headerShown: false }} />
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name={LOGIN_SCREEN} component={LoginScreen} />
+    <Stack.Screen name={SIGN_UP_SCREEN} component={SignUpScreen} />
   </Stack.Navigator>
 );
 
