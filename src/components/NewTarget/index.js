7e04f1bef36a9e4488be294a35f7cdd00acf2dc7
Alan Dours <alan.dours@rootstrap.com>
2021-12-15 14:13:43 -0300

    New Target component

---
+++ src/components/NewTarget/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import { Marker } from 'react-native-maps';
+import { shape, number, func } from 'prop-types';
+
+import targetMarker from 'assets/images/target-marker.png';
+import targetMarkerCircle from 'assets/images/target-marker-circle.png';
+
+import styles from './styles';
+
+const NewTarget = ({ coordinate, changeTargetPosition }) => {
+  return (
+    <Marker
+      coordinate={coordinate}
+      draggable
+      onDragEnd={changeTargetPosition}
+      onPress={e => e.stopPropagation()}>
+      <View style={styles.markerContainer}>
+        <Image source={targetMarkerCircle} style={styles.markerCircle} />
+        <Image source={targetMarker} style={styles.marker} />
+      </View>
+    </Marker>
+  );
+};
+
+NewTarget.propTypes = {
+  coordinate: shape({ latitude: number, longitude: number }).isRequired,
+  changeTargetPosition: func,
+};
+
+export default NewTarget;
